51fae05281c97a319ad49827aeaf6b7befa8764b
Mateusz Czerwinski <mateczerw@gmail.com>
2021-04-30 23:59:40 +0200

    added test

--- src/main/java/com/codewars/vowelCount/Vowels.java
+++ src/main/java/com/codewars/vowelCount/Vowels.java
@@ -3,7 +3,6 @@ package com.codewars.vowelCount;
 public class Vowels {
     public static int getCount(String str) {
         String vowels = "aeiou";
-
         return (int) str.chars()
                 .mapToObj(c -> (char) c)
                 .filter(e -> vowels.contains(String.valueOf(e)))
