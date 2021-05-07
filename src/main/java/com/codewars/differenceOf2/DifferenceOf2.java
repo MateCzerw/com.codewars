c4862299b5931b9b14d9a6fbed5a59cecd4c3754
Mateusz Czerwinski <mateczerw@gmail.com>
2021-05-08 00:14:28 +0200

    update as functional programming

--- src/main/java/com/codewars/differenceOf2/DifferenceOf2.java
+++ src/main/java/com/codewars/differenceOf2/DifferenceOf2.java
@@ -1,32 +1,12 @@
 package com.codewars.differenceOf2;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DifferenceOf2 {
-    public static int[][] twosDifference(int[] array) {
 
-        // sort of elements
-        Set<Integer> sortedSet = new TreeSet<>();
-
-        Arrays.stream(array)
-                .forEach(number -> sortedSet.add(number));
-
-        Set<List<Integer>> setOfPairs = new LinkedHashSet<>();
-        // pairs creation
-        for (Integer number: sortedSet) {
-            if(sortedSet.contains(number + 2)) setOfPairs.add(List.of(number,number + 2));
+        public static int[][] twosDifference(int[] a) {
+            var s = Arrays.stream(a).boxed().collect(Collectors.toSet());
+            return Arrays.stream(a).boxed().filter(x -> s.contains(x + 2)).sorted().map(x -> new int[]{x, x + 2}).toArray(int[][]::new);
         }
-
-        int[][] resultArray = new int[setOfPairs.size()][2];
-
-        // set to 2-dimensional array conversion
-        int iteratorNumber = 0;
-        for (List<Integer> pair : setOfPairs) {
-            resultArray[iteratorNumber][0] = pair.get(0);
-            resultArray[iteratorNumber][1] = pair.get(1);
-            iteratorNumber++;
-        }
-
-        return resultArray;
-    }
 }
