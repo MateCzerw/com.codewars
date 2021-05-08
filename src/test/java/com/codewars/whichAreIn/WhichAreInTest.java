2ba944368aae0e310d6cc497c29ecedf977e8ad6
Mateusz Czerwinski <mateczerw@gmail.com>
2021-05-08 16:16:16 +0200

    Which are in? https://www.codewars.com/kata/550554fd08b86f84fe000a58/

---
+++ src/test/java/com/codewars/whichAreIn/WhichAreInTest.java
@@ -0,0 +1,15 @@
+package com.codewars.whichAreIn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhichAreInTest {
+    @Test
+    public void test1() {
+        String a[] = new String[]{ "arp", "live", "strong" };
+        String b[] = new String[] { "lively", "alive", "harp", "sharp", "armstrong" };
+        String r[] = new String[] { "arp", "live", "strong" };
+        assertArrayEquals(r, WhichAreIn.inArray(a, b));
+    }
+}
