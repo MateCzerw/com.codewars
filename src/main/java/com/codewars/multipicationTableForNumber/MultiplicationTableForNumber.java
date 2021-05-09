73db912078553454b2d59634ac0af8055b6bb472
Mateusz Czerwinski <mateczerw@gmail.com>
2021-05-09 22:56:48 +0200

    refactor

--- src/main/java/com/codewars/multipicationTableForNumber/MultiplicationTableForNumber.java
+++ src/main/java/com/codewars/multipicationTableForNumber/MultiplicationTableForNumber.java
@@ -1,11 +1,13 @@
 package com.codewars.multipicationTableForNumber;
 
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class MultiplicationTableForNumber {
     public static String multiTable(int num) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= 10; i++) {
-            sb.append(String.format("%d * %d = %d\n", i, num, i * num));
-        }
-        return sb.toString().substring(0, sb.length() - 1);
+        return IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> i + " * " + num + " = " + i * num)
+                .collect(Collectors.joining("\n"));
     }
 }
