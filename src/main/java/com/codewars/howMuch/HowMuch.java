863701bd21560c2c8c97b1002fd0d7e86a332f7f
Mateusz Czerwinski <mateczerw@gmail.com>
2021-05-10 22:29:05 +0200

    refactor

--- src/main/java/com/codewars/howMuch/HowMuch.java
+++ src/main/java/com/codewars/howMuch/HowMuch.java
@@ -1,34 +1,18 @@
 package com.codewars.howMuch;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class HowMuch {
     public static String howmuch(int m, int n) {
-        List<Integer[]> result = new LinkedList<>();
-        int newM = m;
-        int newN = n;
-        if(m > n) {
-            newM = n;
-            newN = m;
-        }
-
-        for (int i = newM; i <= newN; i++) {
-            double c = ((double) i - 1) / 9;
-            double b = ((double) i - 2) / 7;
-            if(c % 1 == 0 && b % 1 == 0) result.add(new Integer[]{i,(int) b, (int) c});
-        }
-
-        String resultString =result.stream()
-                                        .map(array -> String
-                                                .format("[M: %d B: %d C: %d]",
-                                                        array[0],
-                                                        array[1],
-                                                        array[2]))
-                                        .collect(Collectors.joining());
-
-        return "[" + resultString + "]";
+        List<String> solutions = IntStream.rangeClosed(Math.min(m, n), Math.max(m, n))
+                .boxed()
+                .filter(f -> f % 9 == 1 && f % 7 == 2)
+                .map(f -> "[M: " + f + " B: " + f / 7 + " C: " + f / 9 + "]")
+                .collect(toList());
+        return   "[" +  String.join((""),solutions) + "]";
 
     }
 }
