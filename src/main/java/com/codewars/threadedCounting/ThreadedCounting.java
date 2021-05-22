b99981270e53a3aee523a81ed403cb7a704e9ce1
Mateusz Czerwinski <mateczerw@gmail.com>
2021-05-22 14:09:19 +0200

    Threaded counting https://www.codewars.com/kata/549e70e994e517ed8b00043e/
    update

--- src/main/java/com/codewars/threadedCounting/ThreadedCounting.java
+++ src/main/java/com/codewars/threadedCounting/ThreadedCounting.java
@@ -1,83 +1,32 @@
 package com.codewars.threadedCounting;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadedCounting {
-    int status = 0;
+    public static void countInThreads(Counter counter) {
 
-    public static void countInThreads(Counter counter) throws InterruptedException {
-        ThreadedCounting obj = new ThreadedCounting();
-        Thread t1 = new Thread(new Print(obj, 0));
-        Thread t2 = new Thread(new Print(obj, 1));
-        Thread t3 = new Thread(new Print(obj, 2));
-        t1.start();
-        t2.start();
-        t3.start();
-        t1.join();
-        t2.join();
-        t3.join();
-    }
-
-
-    static class Print implements Runnable {
-        ThreadedCounting obj;
-        int threadNumber;
-
-        public Print(ThreadedCounting obj, int threadNumber) {
-            this.obj = obj;
-            this.threadNumber = threadNumber;
+        ExecutorService[] execs = new ExecutorService[3];
+        for (int i = 0; i < 3; i++) {
+            execs[i] = Executors.newFixedThreadPool(1);
         }
 
-        @Override
-        public void run() {
-            try {
-
-                if (threadNumber == 0) {
-                    synchronized (obj) {
-                        for (int i = 1; i <= 100; i++) {
-                            while (obj.status != 0 && obj.status != 3) {
-                                obj.wait();
-                            }
-
-//                        System.out.println(0);
-                            if (obj.status == 0)//even sets status to 0 so next one is odd
-                                obj.status = 1;
-                            else//odd sets status to 3 so next one is even
-                                obj.status = 2;
-                            obj.notifyAll();
-                        }
-                    }
-                } else if (threadNumber % 2 != 0) {
-                    synchronized (obj) {
-                        for (int i = 1; i <= 100; i++) {
-                            while (obj.status != 1) {
-                                obj.wait();
-                            }
-
-                            System.out.println(threadNumber);
-                            threadNumber += 2;
-                            obj.status = 3;
-                            //3 decides 0 came after odd, so next one
-                            //after zero is even
-                            obj.notifyAll();
-                        }
-                    }
-                } else {
-                    synchronized (obj) {
-                        for (int i = 1; i <= 100; i++) {
-                            while (obj.status != 2) {
-                                obj.wait();
-                            }
-
-                            System.out.println(threadNumber);
-                            threadNumber += 2;
-                            obj.status = 0;
-                            obj.notifyAll();
-                        }
-                    }
-                }
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+        try {
+            for (int i = 1; i <= 100; i++) {
+                int n = i;
+                Runnable task = () -> counter.count(n);
+                execs[n % 3].submit(task).get();
             }
+
+            for (int i = 0; i < 3; i++) {
+                execs[i].shutdown();
+                execs[i].awaitTermination(1, TimeUnit.SECONDS);
+            }
+        }
+        catch (InterruptedException| ExecutionException e) {
+            throw new Error("can't happen", e);
         }
     }
 }
--- src/test/java/com/codewars/threadedCounting/ThreadedCountingTest.java
+++ src/test/java/com/codewars/threadedCounting/ThreadedCountingTest.java
@@ -11,66 +11,66 @@ import java.util.stream.IntStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ThreadedCountingTest {
-//    @Test
-//    public void the_correct_numbers_are_counted() throws InterruptedException {
-//        Counter counter = new Counter();
-//        ThreadedCounting.countInThreads(counter);
-//
-//        Set<Integer> expected = IntStream.rangeClosed(1, 100)
-//                .boxed()
-//                .collect(Collectors.toSet());
-//        Set<Integer> actual = new HashSet<>(counter.getNumbers());
-//
-//        assertEquals(
-//                expected, actual);
-//    }
-//
-//    @Test
-//    public void the_correct_numbers_are_counted_in_sequence() throws InterruptedException {
-//        Counter counter = new Counter();
-//        ThreadedCounting.countInThreads(counter);
-//
-//        List<Integer> expected = IntStream.rangeClosed(1, 100)
-//                .boxed()
-//                .collect(Collectors.toList());
-//        List<Integer> actual = counter.getNumbers();
-//
-//        assertEquals(
-//                expected, actual);
-//    }
-//
-//    @Test
-//    public void three_threads_are_used() throws InterruptedException {
-//        Counter counter = new Counter();
-//        ThreadedCounting.countInThreads(counter);
-//
-//        assertEquals(
-//                3, counter.getThreadIds().size());
-//    }
-//
-//    @Test
-//    public void numbers_are_in_the_correct_threads() throws InterruptedException {
-//        Counter counter = new Counter();
-//        ThreadedCounting.countInThreads(counter);
-//
-//        List<Integer> expected1 = IntStream.rangeClosed(1, 100)
-//                .filter(i -> i % 3 == 1)
-//                .boxed()
-//                .collect(Collectors.toList());
-//        List<Integer> expected2 = IntStream.rangeClosed(1, 100)
-//                .filter(i -> i % 3 == 2)
-//                .boxed()
-//                .collect(Collectors.toList());
-//        List<Integer> expected3 = IntStream.rangeClosed(1, 100)
-//                .filter(i -> i % 3 == 0)
-//                .boxed()
-//                .collect(Collectors.toList());
-//
-//        assertEquals(
-//                expected1, counter.getNumbersInSameThreadAs(1));
-//        assertEquals(
-//                expected2, counter.getNumbersInSameThreadAs(2));
-//        assertEquals(
-//                expected3, counter.getNumbersInSameThreadAs(3));
-//    }
+    @Test
+    public void the_correct_numbers_are_counted() throws InterruptedException {
+        Counter counter = new Counter();
+        ThreadedCounting.countInThreads(counter);
+
+        Set<Integer> expected = IntStream.rangeClosed(1, 100)
+                .boxed()
+                .collect(Collectors.toSet());
+        Set<Integer> actual = new HashSet<>(counter.getNumbers());
+
+        assertEquals(
+                expected, actual);
+    }
+
+    @Test
+    public void the_correct_numbers_are_counted_in_sequence() throws InterruptedException {
+        Counter counter = new Counter();
+        ThreadedCounting.countInThreads(counter);
+
+        List<Integer> expected = IntStream.rangeClosed(1, 100)
+                .boxed()
+                .collect(Collectors.toList());
+        List<Integer> actual = counter.getNumbers();
+
+        assertEquals(
+                expected, actual);
+    }
+
+    @Test
+    public void three_threads_are_used() throws InterruptedException {
+        Counter counter = new Counter();
+        ThreadedCounting.countInThreads(counter);
+
+        assertEquals(
+                3, counter.getThreadIds().size());
+    }
+
+    @Test
+    public void numbers_are_in_the_correct_threads() throws InterruptedException {
+        Counter counter = new Counter();
+        ThreadedCounting.countInThreads(counter);
+
+        List<Integer> expected1 = IntStream.rangeClosed(1, 100)
+                .filter(i -> i % 3 == 1)
+                .boxed()
+                .collect(Collectors.toList());
+        List<Integer> expected2 = IntStream.rangeClosed(1, 100)
+                .filter(i -> i % 3 == 2)
+                .boxed()
+                .collect(Collectors.toList());
+        List<Integer> expected3 = IntStream.rangeClosed(1, 100)
+                .filter(i -> i % 3 == 0)
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertEquals(
+                expected1, counter.getNumbersInSameThreadAs(1));
+        assertEquals(
+                expected2, counter.getNumbersInSameThreadAs(2));
+        assertEquals(
+                expected3, counter.getNumbersInSameThreadAs(3));
+    }
 }
