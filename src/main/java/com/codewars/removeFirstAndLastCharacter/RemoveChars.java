71f923ca1e0a34a710bdba55d2f8983e8605aef0
Mateusz Czerwinski <mateczerw@gmail.com>
2021-06-07 22:42:39 +0200

    Remove First and Last Character https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/

---
+++ src/main/java/com/codewars/removeFirstAndLastCharacter/RemoveChars.java
@@ -0,0 +1,7 @@
+package com.codewars.removeFirstAndLastCharacter;
+
+public class RemoveChars {
+    public static String remove(String str) {
+        return str.substring(1,str.length() - 1);
+    }
+}
