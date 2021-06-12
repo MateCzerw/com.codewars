0f7132164779868f010399a0a1e2e6d9bab7d75d
Mateusz Czerwinski <mateczerw@gmail.com>
2021-06-12 12:26:39 +0200

    update

--- src/main/java/com/codewars/sortTheOdd/SortTheOdd.java
+++ src/main/java/com/codewars/sortTheOdd/SortTheOdd.java
@@ -1,26 +1,22 @@
 package com.codewars.sortTheOdd;
 
 
-import java.util.*;
+import java.util.PrimitiveIterator;
+import java.util.stream.IntStream;
 
 public class SortTheOdd {
+
     public static int[] sortArray(int[] array) {
-        List<Integer> sortedOddNumbers = new LinkedList<>();
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] % 2 == 1) {
-                sortedOddNumbers.add(array[i]);
-                array[i] = -1;
-            }
-        }
+        PrimitiveIterator.OfInt sortedOdds = IntStream
+                .of(array)
+                .filter(i -> i % 2 == 1)
+                .sorted()
+                .iterator();
 
-        sortedOddNumbers.sort(Comparator.comparing(Integer::intValue));
-
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] == -1) {
-                array[i] = sortedOddNumbers.get(0);
-                sortedOddNumbers.remove(0);
-            }
-        }
-        return array;
+        return IntStream
+                .of(array)
+                .map(i -> i % 2 == 0 ? i : sortedOdds.nextInt())
+                .toArray();
     }
+
 }
