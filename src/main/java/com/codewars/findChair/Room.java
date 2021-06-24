3f264838ea6a2a365d431b39868b4468f2179f19
Mateusz Czerwinski <mateczerw@gmail.com>
2021-06-24 22:48:26 +0200

    The Office V - Find a Chair
    https://www.codewars.com/kata/57f6051c3ff02f3b7300008b

---
+++ src/main/java/com/codewars/findChair/Room.java
@@ -0,0 +1,13 @@
+package com.codewars.findChair;
+
+class Room{
+
+    String occupants;
+    int chairs;
+
+    public Room(String occupants, int chairs) {
+        this.occupants = occupants;
+        this.chairs = chairs;
+    }
+
+}
